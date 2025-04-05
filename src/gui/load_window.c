77ef3745b301e1e35027c639fe2441aeecd506b5
sfarren <sfarren@student.42malaga.com>
2025-04-05 14:01:38 +0200

    Add new tilesets and update readme for Sprout Lands Basic pack
    - Added Tilled Dirt, Tilled Dirt Wide, Tilled Dirt Wide v2, Tilled Dirt v2, Water, Wooden House Roof, Wooden House Walls tilesets.
    - Included a comprehensive readme file outlining licensing, usage, and support information.
    - Updated asset descriptions and credits for proper attribution.

--- src/gui/load_window.c
+++ src/gui/load_window.c
@@ -6,25 +6,34 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 18:57:51 by sfarren           #+#    #+#             */
-/*   Updated: 2025/04/04 13:26:00 by sfarren          ###   ########.fr       */
+/*   Updated: 2025/04/05 13:43:26 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
+#include <X11/keysym.h> // For keysyms
 
 // Function to handle the close event
-static int	close_window(void *mlx)
+int	close_window(void *params[2])
 {
-	mlx_destroy_display(mlx); // Clean up the display
-	free(mlx);                // Free the MiniLibX instance
-	exit(0);                  // Exit the program
+	void	*mlx_win = params[0];
+	void	*mlx = params[1];
+
+	// Destroy the window and clean up MiniLibX resources
+	if (mlx_win)
+		mlx_destroy_window(mlx, mlx_win);
+	if (mlx)
+		mlx_destroy_display(mlx);
+	free(mlx); // Free the MiniLibX instance
+	exit(0);   // Exit the program
 }
 
 // Function to handle key presses
-int	handle_keypress(int keycode, void *mlx)
+int	handle_keypress(int keysym, void *params[2])
 {
-	if (keycode == 65307) // 65307 is the keycode for ESC
-		close_window(mlx);
+	// if (keycode == 65307) // 65307 is the keycode for ESC
+	if (keysym == XK_Escape)
+		close_window(params);
 	return (0);
 }
 
@@ -32,10 +41,14 @@ void	load_window(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	void	*params[2];
 
+	// Initialize MiniLibX
 	mlx = mlx_init();
 	if (mlx == NULL)
 		exit(1);
+
+	// Create a new window
 	mlx_win = mlx_new_window(mlx, 640, 480, "Hello world!");
 	if (mlx_win == NULL)
 	{
@@ -43,11 +56,16 @@ void	load_window(void)
 		exit(1);
 	}
 
+	// Pass both mlx and mlx_win to the event handlers
+	params[0] = mlx_win;
+	params[1] = mlx;
+
 	// Hook the close event (DestroyNotify) to the close_window function
-	mlx_hook(mlx_win, 17, 0, close_window, mlx);
+	mlx_hook(mlx_win, 17, 0, (int (*)(void *))close_window, params);
 
 	// Hook key press events to the handle_keypress function
-	mlx_hook(mlx_win, 2, 1L << 0, handle_keypress, mlx);
+	mlx_hook(mlx_win, 2, 1L << 0, (int (*)(int, void *))handle_keypress, params);
 
+	// Start the MiniLibX event loop
 	mlx_loop(mlx);
 }
